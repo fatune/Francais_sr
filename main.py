a2e8996a25e4375221ccd2cc4c1dd2d2a1bd55b0
fatune <fatune@gmail.com>
2016-11-12 00:12:11 +1100

    Working multiple decks

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request, Markup, redirect
 app = Flask(__name__)
 
-from sr import sr, sr2
+from sr import sr2
 import decks
 
 decks = {"etre": sr2(decks.deck_etre),
-         "Ver" : sr2 (decks.deck_Ver),
-         "Vir" : sr2 (decks.deck_Vir),
-         "faire" : sr2 (decks.deck_faire),
-         "aller" : sr2 (decks.deck_aller),
-         "vouloir" : sr2 (decks.deck_vouloir),
-         "pouvoir" : sr2 (decks.deck_pouvoir),
-         "devoir" : sr2 (decks.deck_devoir),
-       "avoir" : sr2 (decks.deck_avoir)}
+         "Ver" : sr2(decks.deck_Ver),
+         "Vir" : sr2(decks.deck_Vir),
+         "faire" : sr2(decks.deck_faire),
+         "aller" : sr2(decks.deck_aller),
+         "vouloir" : sr2(decks.deck_vouloir),
+         "pouvoir" : sr2(decks.deck_pouvoir),
+         "devoir" : sr2(decks.deck_devoir),
+       "avoir" : sr2(decks.deck_avoir)}
 
 @app.route("/")
 def index():
@@ -23,12 +23,18 @@ def index():
 def members():
     return render_template('decks.html', decks = decks)
 
+@app.route("/back/<string:name>/")
+def go_back(name):
+    decks[name].rate_current_item(0)
+    return redirect("/decks")
+    
+
 @app.route("/deck/<string:name>/")
 def getMember(name):
     item = decks[name].get_next_item()
     title = decks[name].title
     question = item[0]
-    return render_template('deck.html', text = Markup(question), title = title,quest="1")
+    return render_template('deck.html', text = Markup(question), title = title,quest="1",name=name)
 
 
 @app.route("/deck/<string:name>/", methods=['POST'])
@@ -37,9 +43,8 @@ def getMember_post(name):
         if request.form['submit'] == "Show answer":
             item = decks[name].get_last_item_again()
             title = decks[name].title
-            print(name, decks[name],decks[name].get_last_item_again, item)
             answer = item[1]
-            return render_template('deck.html',text = Markup(answer), title = title )
+            return render_template('deck.html',text = Markup(answer), title = title,name=name )
         elif request.form['submit'] == "Bad":
             rate = -1
         elif request.form['submit'] == "Norm":
@@ -50,8 +55,7 @@ def getMember_post(name):
         item = decks[name].get_next_item()
         title = decks[name].title
         question = item[0]
-        return render_template('deck.html', text = Markup(question), title = title,quest="1")
-        #return getMember(name)
+        return getMember(name)
     return "?"
  
 if __name__ == "__main__":
--- sr.py
+++ sr.py
@@ -8,50 +8,10 @@ def return_next_index():
 		i=i+1
 	return int(i) -1
 
-
-class sr:
-	stacks = []
-	current_item = []
-	current_stack = 0
-
-	def __init__(self, to_learn_stack, fresh_stack_capacity = 5, number_of_stacks = 4):
-		for i in range(number_of_stacks):
-			self.stacks.append([])
-			self.to_learn_stack = to_learn_stack
-			self.fresh_stack_capacity = fresh_stack_capacity
-	
-	def get_next_item(self):
-		# add new element in fresh stack if there any to add
-		if not len(self.stacks[0])>self.fresh_stack_capacity and len(self.to_learn_stack) > 0:
-			self.stacks[0].append(self.to_learn_stack.pop(0))
-				
-		# chose next item
-		while True:
-			idx = return_next_index()
-			for current_stack in range(len(self.stacks)):
-				if idx < len(self.stacks[current_stack]): break
-				idx -= len(self.stacks[current_stack])
-			if len(self.stacks[current_stack])>idx: 
-				break # random index is good big
-		self.current_item = self.stacks[current_stack].pop(idx)
-		self.current_stack = current_stack
-		return self.current_item
-
-	def get_last_item_again(self):
-		return self.current_item
-
-	def rate_current_item(self, rate):
-		current_stack = self.current_stack
-		if rate == +1 and current_stack < (len(self.stacks)-1): current_stack += 1
-		if rate == -1 and current_stack > 0 : current_stack -= 1
-		self.stacks[current_stack].append(self.current_item)
-
 class sr2:
-	stacks = []
-	current_item = []
-	current_stack = 0
-
-	def __init__(self, deck, fresh_stack_capacity = 5, number_of_stacks = 4):
+	def __init__(self, deck, fresh_stack_capacity = 5, number_of_stacks = 1):
+                self.__stacks = []
+                self.__current_item = None
 		self.name = deck["name"]
 		self.title = deck["title"]
 		self.template =  deck["template"]
@@ -59,33 +19,37 @@ class sr2:
 		self.aparser = deck["aparser"]
 		self.to_learn_stack = deck["questions"]
 		for i in range(number_of_stacks):
-			self.stacks.append([])
+			self.__stacks.append([])
 		self.fresh_stack_capacity = fresh_stack_capacity
 	
 	def get_next_item(self):
+                if self.__current_item:
+                    self.rate_current_item(0)
+
 		# add new element in fresh stack if there any to add
-		if not len(self.stacks[0])>self.fresh_stack_capacity and len(self.to_learn_stack) > 0:
-			self.stacks[0].append(self.to_learn_stack.pop(0))
+		if not len(self.__stacks[0])>self.fresh_stack_capacity and len(self.to_learn_stack) > 0:
+			self.__stacks[0].append(self.to_learn_stack.pop(0))
 				
 		# chose next item
 		while True:
 			idx = return_next_index()
-			for current_stack in range(len(self.stacks)):
-				if idx < len(self.stacks[current_stack]): break
-				idx -= len(self.stacks[current_stack])
-			if len(self.stacks[current_stack])>idx: 
+			for current_stack in range(len(self.__stacks)):
+				if idx < len(self.__stacks[current_stack]): break
+				idx -= len(self.__stacks[current_stack])
+			if len(self.__stacks[current_stack])>idx: 
 				break # random index is good big
-		self.current_item = self.stacks[current_stack].pop(idx)
+		self.__current_item = self.__stacks[current_stack].pop(idx)
 		self.current_stack = current_stack
-		return [self.qparser(self.current_item[0],self.current_item[1]),
-                        self.aparser(self.current_item[0],self.current_item[1])]
+		return self.get_last_item_again()
 
 	def get_last_item_again(self):
-		return [self.qparser(self.current_item[0],self.current_item[1]),
-                        self.aparser(self.current_item[0],self.current_item[1])]
+		return [self.qparser(self.__current_item[0],self.__current_item[1]),
+                        self.aparser(self.__current_item[0],self.__current_item[1])]
 
 	def rate_current_item(self, rate):
 		current_stack = self.current_stack
-		if rate == +1 and current_stack < (len(self.stacks)-1): current_stack += 1
+		if rate == +1 and current_stack < (len(self.__stacks)-1): current_stack += 1
 		if rate == -1 and current_stack > 0 : current_stack -= 1
-		self.stacks[current_stack].append(self.current_item)
+		self.__stacks[current_stack].append(self.__current_item)
+		self.__current_item = None
+
